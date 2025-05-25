a57ea17889513e5a9846088609011776e7eb688e
delmaSong <sdy2856@gmail.com>
2025-05-25 16:31:57 +0900

    Swift Concurrency 사용위해 미니멈 버전 설정

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
 	name: "AsyncReactorKit",
+	platforms: [
+		.iOS(.v13)
+	],
     products: [
         .library(
             name: "AsyncReactorKit",
